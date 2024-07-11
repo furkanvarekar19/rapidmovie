0028c563732134d5f6c272674a2033217dd3388c
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-11 19:36:49 +0530

    update

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,27 +1,52 @@
+
 import './App.css';
-import MovieItem from './component/MovieItem';
-import Movies from './component/Movies';
-import SearchBar from './component/SearchBar';
-import { Route, Routes} from 'react-router-dom'
-import { useState } from 'react';
+import MovieList from "./components/MovieList"
+import {Routes,Route} from "react-router-dom"
+import MovieItem from "./components/MovieItem"
+import MovieVideo from "./components/MovieVideo"
+import axios from "axios"
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [movieData, setMovieData] = useState([]);
 
-  const [datarecipe , setdatarecipe] = useState();
-  const Addmovies  = (i) =>{
-    setdatarecipe([...datarecipe , i])
+  console.log(movieData, "movieData");
+  async function MoviApiCalling() {
+
+    // const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  // url: 'https://movies-api14.p.rapidapi.com/search',
+  params: {
+    query: 'breaking bad'
+  },
+  headers: {
+    // 'x-rapidapi-key':'60c19c5270msh31e46635766ef45p19c71cjsn47ac3532a6fe',
+    'x-rapidapi-host': 'movies-api14.p.rapidapi.com'
   }
- 
+};
+
+try {
+	const response = await axios.request(options);
+	setMovieData(response.data.contents);
+} catch (error) {
+	console.error(error);
+}
+  }
+useEffect(()=>{
+  MoviApiCalling();
+},[])
+
   return (
     <div className="App">
-      <Routes>
-       <Route path='/' element= {<Movies Addmovies ={Addmovies} />}/>
-       <Route path='/MovieItem/:id' element= {<MovieItem i ={datarecipe} />}/>
-       <Route path='/SearchBar' element= {<SearchBar/>}/>
+      <Routes >
+       <Route path="/" element={<MovieList movieData={movieData}/>}/> 
+       <Route path="/movieitem/:id" element={<MovieItem movieData={movieData}/>}/> 
+       <Route path='/movievideo/:id'element={<MovieVideo movieData={movieData}/>}/>
 
       </Routes>
-       
-     
     </div>
   );
 }
---
+++ movie/src/components/MovieItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+const MovieItem = ({movieData,index}) => {
+    const {id}=useParams();
+    const movieDatas=movieData[id];
+
+    console.log(movieDatas,"movieDatas")
+
+  return (
+    <div>
+        <div>
+            {
+            <div className='mainimg' >
+             <div className='textImg'>
+            <div className='moviesTtl'>
+                <h4>{movieDatas.contentType}</h4>
+                <h2>{movieDatas.original_title}</h2>
+            </div>
+             <p className='overvw'>{movieDatas.overview}</p>
+        
+              <div className='mainimg2'>
+                <p>Release date <span>{movieDatas.release_date}</span></p>
+                <p>Ratings <span>{movieDatas.vote_average}</span></p>
+                <p><span>{movieDatas.vote_count}</span></p>
+            </div>
+              <div className='CDT'>
+                <h3>{movieDatas.genres[0]}</h3>
+                <h3>{movieDatas.genres[1]}</h3>
+                <h3>{movieDatas.genres[2]}</h3>
+                </div>
+                <Link to={`/movievideo/${index}`}>
+                <div>play</div>
+                </Link>
+               
+              </div>
+            </div>  
+
+            }
+        </div>
+    </div>
+  )
+}
+
+export default MovieItem;
